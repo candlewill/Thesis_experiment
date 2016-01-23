5f3a71126b7afe22bf3ad1121765cbefcc1a1f78
HYC <yunchaohe@gmail.com>
2016-01-23 21:31:45 +0800

    add CVAT corpus with labels

--- lexicon_based_method.py
+++ lexicon_based_method.py
@@ -72,11 +72,11 @@ if __name__ == '__main__':
 
     ####################### Hyper-parameters #########################
     using_extended_lexicon = False  # 'True' or 'False'
-    option = 'V'  # 'V' or 'A'
+    option = 'A'  # 'V' or 'A'
     mean_method = 'tf_geo'  # values: 'tf_geo', 'tf_mean'
     sigma = 1.5  # values: '1.0', '1.5', '2.0'
     tokenizer = 'ckip'  # values: 'jieba', 'ckip'
-    categorical = 'book'  # values: 'all', "book", "car", "laptop", "hotel", "news", "political"
+    categorical = 'political'  # values: 'all', "book", "car", "laptop", "hotel", "news", "political"
     ##################################################################
 
     if tokenizer == 'ckip':
