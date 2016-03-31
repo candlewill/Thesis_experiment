5f14902cf1c6e16f635d0177271fd931e21181b6
Yunchao He <yunchaohe@gmail.com>
2016-03-31 22:37:25 +0800

    add new data

--- build_tokenized_texts.py
+++ build_tokenized_texts.py
@@ -1,5 +1,5 @@
 from load_data import load_CVAT_2
-filename = './resources/CVAT2.0(sigma=1.5).csv'
+filename = './resources/CVAT (utf-8).csv'
 texts, valence, arousal = load_CVAT_2(filename, categorical="all")
 len_text = []
 from CKIP_tokenizer import segsentence
@@ -10,5 +10,5 @@ for idx, i in enumerate(texts):
     out.append(" ".join(segsentence(i)))
     # len_text.append(len(.split()))
 from save_data import dump_picle
-dump_picle(out, "tokenized_texts_(old).p")
+dump_picle(out, "tokenized_texts_(newest3.31).p")
 print("The tokenized text is saved.")
